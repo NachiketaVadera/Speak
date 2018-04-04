aa74ce7594f3bd66851d0cb019cac49a298d46f1
Nachiketa Vadera <personal.nachiketa@gmail.com>
2018-04-04 14:23:09 +0530

    Changes made: Removed Service Removed Options Menu Added a transparent activity and shifted the intent-filter from Launcher activity to this activity TextToSpeech will run without displaying the app.

--- app/src/main/java/util/android/speak/MainActivity.java
+++ app/src/main/java/util/android/speak/MainActivity.java
@@ -1,9 +1,8 @@
 package util.android.speak;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -17,37 +16,30 @@ public class MainActivity extends AppCompatActivity implements
     EditText editText = null;
     LinearLayout linearLayout = null;
     TextToSpeech textToSpeech = null;
-    String receivedText = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Bundle bundle = getIntent().getExtras();
-
         linearLayout = (LinearLayout) findViewById(R.id.llParent);
         editText = (EditText) findViewById(R.id.etText);
+
         textToSpeech = new TextToSpeech(this,this);
         textToSpeech.setLanguage(Locale.ENGLISH);
 
         linearLayout.setOnTouchListener(this);
 
-        if(bundle != null){
-            receivedText = bundle.getString(Intent.EXTRA_PROCESS_TEXT);
-            editText.setText(receivedText);
-        }
-
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        textToSpeech.speak(editText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+        textToSpeech.speak(editText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null,"onTouchTextToSpeech");
         return false;
     }
 
     @Override
     public void onInit(int status) {
-        textToSpeech.speak(receivedText,TextToSpeech.QUEUE_FLUSH,null);
+        // unused method
     }
 }
