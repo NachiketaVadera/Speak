1c1a91db1c16eae93eac20024763f5ea68168464
NachiketaVadera <personal.nachiketa@gmail.com>
2019-10-08 23:27:37 +0530

    updated minor things before making big changes
    Signed-off-by: NachiketaVadera <personal.nachiketa@gmail.com>

--- app/src/main/java/util/android/speak/MainActivity.java
+++ app/src/main/java/util/android/speak/MainActivity.java
@@ -2,45 +2,41 @@ package util.android.speak;
 
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements
-        View.OnTouchListener, TextToSpeech.OnInitListener {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
-    EditText editText = null;
-    LinearLayout linearLayout = null;
-    TextToSpeech textToSpeech = null;
+    public static final String TAG = "__Speak__";
+
+    private EditText editText = null;
+    private TextToSpeech textToSpeech = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        linearLayout = findViewById(R.id.llParent);
+        findViewById(R.id.llParent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textToSpeech.speak(editText.getText().toString(),
+                        TextToSpeech.QUEUE_FLUSH, null, "onTouchTextToSpeech");
+            }
+        });
         editText = findViewById(R.id.etText);
 
-        textToSpeech = new TextToSpeech(this,this);
+        textToSpeech = new TextToSpeech(this, this);
         textToSpeech.setLanguage(Locale.ENGLISH);
-
-        linearLayout.setOnTouchListener(this);
-
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        textToSpeech.speak(editText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null,"onTouchTextToSpeech");
-        return false;
     }
 
     @Override
     public void onInit(int status) {
-        // unused method
+        Log.i(TAG, "onInit: TextToSpeech Init with status " + status);
     }
 }
